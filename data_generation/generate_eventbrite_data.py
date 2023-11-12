dc38319fd33451fa2ddecf2b65865db402f5e981
Alexandra Rollings <alexandrarollings@Alexandras-MacBook-Pro.local>
2023-11-12 14:34:32 +0100

    Current generation scripts

--- data_generation/generate_eventbrite_data.py
+++ data_generation/generate_eventbrite_data.py
@@ -12,16 +12,17 @@ tech_jobs = ['Software Engineer', 'Data Scientist', 'IT Consultant', 'Systems An
 
 # Generate pseudo data
 data = {
-    'profile': [fake.simple_profile() for _ in range(num_responses)],
-    'profile_name': [fake.name() for _ in range(num_responses)],
-    'profile_email': [fake.email() for _ in range(num_responses)],
+    # 'profile': [fake.simple_profile() for _ in range(num_responses)],
+    # 'profile_name': [fake.name() for _ in range(num_responses)],
+    # 'profile_email': [fake.email() for _ in range(num_responses)],
     'job_title': np.random.choice(tech_jobs, num_responses),
+    'age': np.random.choice(tech_jobs, num_responses),
     'checked_in': np.random.choice([True, False], num_responses, p=[0.8, 0.2]),
     'cancelled': np.random.choice([True, False], num_responses, p=[0.8, 0.2]),
     'refunded': np.random.choice([True, False], num_responses, p=[0.8, 0.2]),
     'affiliate': [fake.company() for _ in range(num_responses)],
-    'event_id': [fake.random_int(min=1000, max=9999) for _ in range(num_responses)],
-    'order_id': [fake.random_int(min=1000, max=9999) for _ in range(num_responses)]
+    # 'event_id': [fake.random_int(min=1000, max=9999) for _ in range(num_responses)],
+    # 'order_id': [fake.random_int(min=1000, max=9999) for _ in range(num_responses)]
 }
 
 df = pd.DataFrame(data)
